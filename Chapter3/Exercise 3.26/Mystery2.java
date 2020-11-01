6fe5e0680bb31796b5f37cdadbb0b42592057b97
Augusto Loose Bellinaso <53017411+augustobellinaso@users.noreply.github.com>
2020-11-01 13:06:27 -0300

    Exercise 3.26

---
+++ Chapter3/Exercise 3.26/Mystery2.java
@@ -0,0 +1,22 @@
+/*
+Augusto Loose Bellinaso
+ */
+public class Mystery2 {
+	
+	public static void main(String[] args) {
+		
+		int row = 10;
+		
+		while (row >= 1) {
+			int column = 1;
+			
+			while (column <= 10) {
+				System.out.print(row % 2 == 1 ? "<" : ">");
+				++column;
+			}
+			--row;
+			System.out.println();
+		}
+	}
+
+}
