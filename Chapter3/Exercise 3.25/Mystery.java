3e53d3ffa7a6824b461247cd353bf7717a4299dc
Augusto Loose Bellinaso <53017411+augustobellinaso@users.noreply.github.com>
2020-11-01 13:06:23 -0300

    Exercise 3.25

---
+++ Chapter3/Exercise 3.25/Mystery.java
@@ -0,0 +1,16 @@
+/*
+Augusto Loose Bellinaso
+ */
+public class Mystery {
+	
+	public static void main(String[] args) {
+		
+		int count = 1;
+		
+		while(count <= 10) {
+			System.out.println(count % 2 == 1 ? "****" : "++++++++");
+			++count;
+		}
+	}
+
+}
